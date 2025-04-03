22ee52ca16d8840d6b3c5524382bcb101f6d5f7f
Uri <78451970+Yuri1325@users.noreply.github.com>
2025-04-03 12:40:31 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,23 @@
 import socket
 import os
 import platform
-
+from _thread import *
 def shutdown_server():
     # Create TCP socket
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("0.0.0.0", 5050))  # Listen on all interfaces
     server.listen(1)
     print("[SERVER LISTENING] on port 5050...")
-
+    def threadedClient(conn):
+        while True:
+            clientMessage = conn.recv(64).decode('utf-8)
+        
     while True:
-        client_socket, addr = server.accept()
+        conn, addr = server.accept()
         print(f"Connection from {addr}")
         
         # Receive shutdown command
-        data = client_socket.recv(1024).decode("utf-8")
-        if data.lower() == "shutdown":
-            # Platform-specific shutdown commands
-            if platform.system() == "Windows":
-                os.system("shutdown /s /t 1")
-            elif platform.system() in ["Linux", "Darwin"]:
-                os.system("sudo shutdown -h now")
-            client_socket.send("Shutdown initiated".encode("utf-8"))
-            break
+       
         client_socket.close()
     server.close()
 
